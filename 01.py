aaa83f7d8a1071697ac4b78b6bdec8d432500ece
Algirdas Neniskis <algirdas.neniskis@gmail.com>
2025-12-02 22:11:56 +0200

    Problem 1 - part 2

--- 01.py
+++ 01.py
@@ -3,12 +3,23 @@ def main():
         rotations = [int(line.replace("R", "").replace("L", "-")) for line in file.readlines()]
 
     dial = 50
+    exact = 0
     total = 0
     for r in rotations:
-        dial += r
-        total += dial % 100 == 0
+        if dial == 0 and r < 0:
+            dial = 100
 
+        div, dial = divmod(dial + r, 100)
+
+        exact += dial % 100 == 0
+
+        total += abs(div)
+        if dial == 0 and r < 0:
+            total += 1
+
+    print(exact)
     print(total)
+
 
 if __name__ == "__main__":
     main()
