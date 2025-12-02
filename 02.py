da4a61512e25a92a53a0bd18a69671b48655d929
Algirdas Neniskis <algirdas.neniskis@gmail.com>
2025-12-02 22:42:30 +0200

    Problem 2 - part 2

--- 02.py
+++ 02.py
@@ -1,20 +1,30 @@
+import re
+
+SIMPLE_PATTERN = re.compile(r"^(.+)\1{1}$")
+COMPLEX_PATTERN = re.compile(r"^(.+)\1+$")
+
+
 def main():
     with open("data/02.txt") as file:
         ranges = file.readline().split(",")
 
+    simple = 0
     total = 0
     for r in ranges:
         low, high = (int(x) for x in r.split("-"))
 
         for i in range(low, high + 1):
             s = str(i)
-            l = len(s)
-            if l % 2 != 0:
-                continue
-            if s[:l // 2] == s[l // 2:]:
+
+            if SIMPLE_PATTERN.match(s):
+                simple += i
+
+            if COMPLEX_PATTERN.match(s):
                 total += i
 
+    print(simple)
     print(total)
+
 
 if __name__ == "__main__":
     main()
