dfe32b9c93328070648cae0acd713d28093ed32e
Algirdas Neniskis <algirdas.neniskis@gmail.com>
2025-12-03 20:13:34 +0200

    Problem 3 - part 2

--- 03.py
+++ 03.py
@@ -11,6 +11,22 @@ def main():
 
     print(total)
 
+    total = 0
+    for b in banks:
+        left = 0
+        right = len(b) - 11
+        digits = []
+        for _ in range(12):
+            d = max(b[left:right])
+            digits.append(d)
+
+            left += b[left:right].index(d) + 1
+            right += 1
+
+        total += int("".join(str(x) for x in digits))
+
+    print(total)
+
 
 if __name__ == "__main__":
     main()
