f8504d84254efd824257b6240837754c3bad8c4c
Algirdas Neniskis <algirdas.neniskis@gmail.com>
2025-12-04 15:49:44 +0200

    Problem 4 - part 2

--- 04.py
+++ 04.py
@@ -2,26 +2,58 @@ EMPTY = "."
 ROLL = "@"
 
 
+def count_neighbor_rolls(grid, x, y):
+    min_x = max(x - 1, 0)
+    max_x = min(x + 2, len(grid))
+
+    min_y = max(y - 1, 0)
+    max_y = min(y + 2, len(grid[x]))
+
+    return [item for row in grid[min_x:max_x] for item in row[min_y:max_y]].count(ROLL) - (grid[x][y] == ROLL)
+
+
+def remove(grid, x, y):
+    if grid[x][y] == EMPTY:
+        return 0
+
+    if count_neighbor_rolls(grid, x, y) >= 4:
+        return 0
+
+    grid[x][y] = EMPTY
+
+    min_x = max(x - 1, 0)
+    max_x = min(x + 2, len(grid))
+
+    min_y = max(y - 1, 0)
+    max_y = min(y + 2, len(grid[x]))
+
+    total_removed = 1
+    for i in range(min_x, max_x):
+        for j in range(min_y, max_y):
+            total_removed += remove(grid, i, j)
+
+    return total_removed
+
+
 def main():
     with open("data/04.txt") as file:
         grid = [[x for x in line] for line in file.read().splitlines()]
 
-    total = 0
+    accessible = 0
     for i in range(len(grid)):
-        min_x = max(i - 1, 0)
-        max_x = min(i + 2, len(grid))
         for j in range(len(grid[i])):
             if grid[i][j] == EMPTY:
                 continue
+            accessible += count_neighbor_rolls(grid, i, j) < 4
 
-            min_y = max(j - 1, 0)
-            max_y = min(j + 2, len(grid[i]))
+    print(accessible)
 
-            count_rolls = [item for row in grid[min_x:max_x] for item in row[min_y:max_y]].count(ROLL)
+    total_removed = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            total_removed += remove(grid, i, j)
 
-            total += count_rolls < 5
-
-    print(total)
+    print(total_removed)
 
 
 if __name__ == "__main__":
