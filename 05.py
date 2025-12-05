f87599ee8eaaddb6ff6c4974002464ae4d260085
Algirdas Neniskis <algirdas.neniskis@gmail.com>
2025-12-05 18:20:34 +0200

    Problem 5 - part 2

--- 05.py
+++ 05.py
@@ -1,3 +1,20 @@
+def consolidate_ranges(ranges: list[range]):
+    ranges_copy = [r for r in ranges]
+    ranges.clear()
+
+    while ranges_copy:
+        r = ranges_copy.pop(0)
+        for other_r in ranges_copy:
+            min_start = min(r.start, other_r.start)
+            max_stop = max(r.stop, other_r.stop)
+            if len(r) + len(other_r) >= max_stop - min_start:
+                ranges_copy.remove(other_r)
+                ranges_copy.append(range(min_start, max_stop))
+                break
+        else:
+            ranges.append(r)
+
+
 def main():
     with open("data/05.txt") as file:
         r, i = file.read().split("\n\n")
@@ -10,6 +27,10 @@ def main():
         fresh += any(ingredient in r for r in ranges)
 
     print(fresh)
+
+    consolidate_ranges(ranges)
+
+    print(sum(len(x) for x in ranges))
 
 
 if __name__ == "__main__":
