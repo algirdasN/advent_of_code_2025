fe6bed59c28220ed3bb71bea2fdbbf09cad08136
Algirdas Neniskis <algirdas.neniskis@gmail.com>
2025-12-08 22:10:54 +0200

    Problem 8 - part 2

--- 08.py
+++ 08.py
@@ -1,4 +1,19 @@
 import math
+from copy import copy
+
+
+def consolidate_clusters(clusters):
+    clusters_copy = copy(clusters)
+    clusters.clear()
+    while clusters_copy:
+        cluster = clusters_copy.pop()
+        for other in clusters_copy:
+            if cluster & other:
+                clusters_copy.remove(other)
+                clusters_copy.append(cluster | other)
+                break
+        else:
+            clusters.append(cluster)
 
 
 def main():
@@ -24,20 +39,25 @@ def main():
         else:
             clusters.append(set(pair))
 
-    joined_clusters = []
-    while clusters:
-        cluster = clusters.pop()
-        for other in clusters:
-            if cluster & other:
-                clusters.remove(other)
-                clusters.append(cluster | other)
-                break
-        else:
-            joined_clusters.append(cluster)
-
-    sorted_clusters = sorted(joined_clusters, key=len, reverse=True)
+    consolidate_clusters(clusters)
+    sorted_clusters = sorted(clusters, key=len, reverse=True)
 
     print(len(sorted_clusters[0]) * len(sorted_clusters[1]) * len(sorted_clusters[2]))
+
+    while len(clusters) > 1 or len(clusters[0]) < len(junctions):
+        pair = shortest_distance[connections]
+        for c in clusters:
+            if pair[0] in c or pair[1] in c:
+                c.add(pair[0])
+                c.add(pair[1])
+                consolidate_clusters(clusters)
+                break
+        else:
+            clusters.append(set(pair))
+
+        connections += 1
+
+    print(shortest_distance[connections - 1][0][0] * shortest_distance[connections - 1][1][0])
 
 
 if __name__ == "__main__":
