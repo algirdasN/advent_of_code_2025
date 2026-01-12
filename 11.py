a9f48056eff6165bc6ef31eb2a5be308cc15348a
algirdasN <61432976+algirdasN@users.noreply.github.com>
2026-01-12 15:52:06 +0200

    Problem 11 - simplify

--- 11.py
+++ 11.py
@@ -5,30 +5,23 @@ DAC = "dac"
 FFT = "fft"
 
 
-def walk_simple(connections, curr, end):
-    if curr == end:
-        return 1
-
-    return sum(walk_simple(connections, x, end) for x in connections[curr])
-
-
-def walk_complex(connections, curr, end, valid_paths):
+def walk(connections, curr, end, valid_paths):
     if curr == end:
         return 1
 
     if curr not in valid_paths:
-        valid_paths[curr] = sum(walk_complex(connections, x, end, valid_paths) for x in connections[curr])
+        valid_paths[curr] = sum(walk(connections, x, end, valid_paths) for x in connections[curr])
 
     return valid_paths[curr]
 
 
 def path_svr_to_out(connections):
-    svr_to_dac = walk_complex(connections, SVR, DAC, {FFT: 0, OUT: 0})
-    svr_to_fft = walk_complex(connections, SVR, FFT, {DAC: 0, OUT: 0})
-    dac_to_fft = walk_complex(connections, DAC, FFT, {OUT: 0})
-    fft_to_dac = walk_complex(connections, FFT, DAC, {OUT: 0})
-    dac_to_out = walk_complex(connections, DAC, OUT, {FFT: 0})
-    fft_to_out = walk_complex(connections, FFT, OUT, {DAC: 0})
+    svr_to_dac = walk(connections, SVR, DAC, {FFT: 0, OUT: 0})
+    svr_to_fft = walk(connections, SVR, FFT, {DAC: 0, OUT: 0})
+    dac_to_fft = walk(connections, DAC, FFT, {OUT: 0})
+    fft_to_dac = walk(connections, FFT, DAC, {OUT: 0})
+    dac_to_out = walk(connections, DAC, OUT, {FFT: 0})
+    fft_to_out = walk(connections, FFT, OUT, {DAC: 0})
 
     return svr_to_dac * dac_to_fft * fft_to_out + svr_to_fft * fft_to_dac * dac_to_out
 
@@ -42,7 +35,7 @@ def main():
         split = r.split(" ")
         connections[split[0][:-1]] = split[1:]
 
-    print(walk_simple(connections, YOU, OUT))
+    print(walk(connections, YOU, OUT, {}))
     print(path_svr_to_out(connections))
 
 
